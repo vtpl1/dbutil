c5c1e87ae7b5f35c50c2aecf1e7766338a9cd091
Monotosh Das <monotosh.das@videonetics.com>
2023-12-18 00:53:17 +0530

    starting point

---
+++ include/dbutil.h
@@ -0,0 +1,10 @@
+// *****************************************************
+//    Copyright 2023 Videonetics Technology Pvt Ltd
+// *****************************************************
+
+
+#pragma once
+#ifndef dbutil_h
+#define dbutil_h
+
+#endif	// dbutil_h
